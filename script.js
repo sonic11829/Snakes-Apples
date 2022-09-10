38147bb1d8c80697b89a18050e5a89fd5068bf2d
sonic11829 <sethneal15@ymail.com>
2022-09-10 16:47:28 -0400

    got my plan setup in pseudo

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('snaAake');
+console.log('slippery snaaake');
+//make the grid that the snake and apples will traverse.
+
+//make the snake and give it functionallity.
+
+//make the apples and make them appear in random locations on the grid when an apple has beem devoured.
+
+//make the snake grow everytime an apple has eaten.
+
+//display a score for every apple that gets eaten & or display max length achieved.
+
+//make it so you can change the color of your slippery snaaake.
