dc0705046c12e6be6fdc3b1361e412530639b22e
sonic11829 <sethneal15@ymail.com>
2022-09-13 22:45:42 -0400

    appended snake and gave him controls  attempted to append apple and snake dissappeared and food wont show

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 console.log('snaAake');
 console.log('slippery snaaake');
 //make the grid that the snake and apples will traverse.
@@ -13,19 +14,106 @@ console.log('slippery snaaake');
 
 //make it so you can change the color of your slippery snaaake.
 
- let lastRenderTime = 0
- const snake_speed = 1
+let lastRenderTime = 0;
+const snakeSpeed = 3;
+const field = document.querySelector('.field');
 
 function main(currentTime) {
-    window.requestAnimationFrame(main)
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / snake_speed) return
+    window.requestAnimationFrame(main);
+    const timeAfterLastRender = (currentTime - lastRenderTime) / 1000;
+    if (timeAfterLastRender < 1 / snakeSpeed) return;
 
-    console.log('render')
-    lastRenderTime = currentTime
+    lastRenderTime = currentTime;
 
-    update()
-    draw()
+    update();
+    draw();
 }
 
-window.requestAnimationFrame(main)
+window.requestAnimationFrame(main);
+
+function update(){
+    updateSnake();
+    updateApple();
+}
+
+function draw(){
+    drawSnake(field);
+    drawApple(field);
+}
+
+//render the snake
+
+const snakeSkin = [{ x: 9, y: 9}];
+
+function updateSnake(){
+    const snakeControls = getSnakeControls();
+    drawSnake(field);
+    for (let i = snakeSkin.length - 2; i >= 0; i--) {
+        snakeSkin[i + 1] = { ...snakeSkin[i] };
+    }
+
+    snakeSkin[0].x += snakeControls.x;
+    snakeSkin[0].y += snakeControls.y;
+}
+
+function drawSnake(field){
+    field.innerHTML = "";
+    snakeSkin.forEach(skin => {
+        const snakeEl = document.createElement('div');
+        snakeEl.style.gridRowStart = skin.y;
+        snakeEl.style.gridColumnStart = skin.x;
+        snakeEl.classList.add('snake');
+        field.appendChild(snakeEl);
+    })
+    
+}
+
+//render apple
+
+let apple = { x: 1, y: -1};
+
+function updateApple() {
+    drawApple(field);
+}
+
+function drawApple(field) {
+    field.innerHTML = "";
+    const appleEl = document.createElement('div');
+    appleEl.style.gridRowStart = apple.y;
+    appleEl.style.gridColumnStart = apple.x;
+    appleEl.classList.add('apple');
+    field.appendChild(appleEl);
+}
+
+//give controls to the player
+
+let snakeControls = { x: 0, y: 0 };
+let lastSnakeDirection = { x: 0, y: 0 };
+
+function getSnakeControls() {
+    lastSnakeDirection = snakeControls;
+    return snakeControls;
+}
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (lastSnakeDirection.y !== 0) break;
+            snakeControls = { x: 0, y: -1};
+            break;
+        case 'ArrowDown':
+            if (lastSnakeDirection.y !== 0) break;
+            snakeControls = { x: 0, y: 1};
+            break;
+        case 'ArrowLeft':
+            if (lastSnakeDirection.x !== 0) break;
+            snakeControls = { x: -1, y: 0};
+            break;
+        case 'ArrowRight':
+            if (lastSnakeDirection.x !== 0) break;
+            snakeControls = { x: 1, y: 0};
+            break;
+    }
+})
+
+
